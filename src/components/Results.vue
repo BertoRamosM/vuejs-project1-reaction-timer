74f454215aa0667fcf07c8950559a30de1efd947
ARamos <albertramos902@gmail.com>
2024-05-27 11:09:23 +0100

    added some styles

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,13 +1,32 @@
  <template>
      <p >Reaction time: {{ reactionTime }}ms</p>
+     <p class="rank">{{ rank }}</p>
  </template>
  
  <script>
- export default {
- props : ['reactionTime']
- }
+export default {
+  props: ['reactionTime'],
+  data() {
+    return {
+      rank: null
+    }
+  },
+  mounted() {
+    if (this.reactionTime < 250) {
+      this.rank = "Ninja fingers!"
+    } else if (this.reactionTime < 400) {
+      this.rank = "Rapid reflexes"
+    } else {
+      this.rank = "Snail pace..."
+    }
+  }
+}
  </script>
  
  <style>
- 
+ .rank{
+  font-size: 1,4rem;
+  color: #0faf87;
+  font-weight: bold;
+ }
  </style>
